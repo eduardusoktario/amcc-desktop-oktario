83842b9516607fff6bca6c5bf233dc7b6837da4d
oktario <oktario.11@students.amikom.ac.id>
2019-12-21 13:35:45 +0700

    belajar tupple

---
+++ pertemuan3.py
@@ -0,0 +1,7 @@
+#Contoh sederhana pembuatan list pada bahasa pemrograman Python
+
+list1 = ['kimia', 'fisika', 1993, 2017]
+list2 = [1, 2, 3, 4, 5, 6, 7]
+
+print ("list1[0]: ", list1[0])
+print ("list2[1:5]: ", list2[1:5])
