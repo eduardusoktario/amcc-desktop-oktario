18d755ed19a1aea04bf6d7de911dbed0a9abf26c
oktario <oktario.11@students.amikom.ac.id>
2019-12-21 14:22:41 +0700

    belajar dictionary

---
+++ pertemuan4.py
@@ -0,0 +1,18 @@
+#Dictionary kuncinya ada di key sama value yang dipisah :
+
+#dict = {'Name':'Zara','Age':7,'Class':'First'} #Deklarasi Dictionary Python
+#dict['Age']=8 #Mengganti Data di Dictionary Python
+#dict['School']="DPS School" #Menambah Data Baru di Dictionary Python
+#del dict['Name'] #Menghapus Elemen Dalam Dictionary Python
+#dict.clear() #Menghapus Dictionary Dalam Python
+
+#print ("dict['Name']: ", dict['Name'])
+#print ("dict['Age']: ", dict['Age'])
+#print ("dict['School']: ", dict['School'])
+
+#Latihan Dictionary
+dict = {'Mangga':'5 kg', 'Jeruk':'10 kg', 'HargaMangga':'Rp 10.000', 'HargaJeruk':'Rp 1.000'}
+
+print ("Andi Beli Mangga ", dict['Mangga'], ", Jeruk ", dict['Jeruk'])
+print ("Harga Mangga = ", dict['HargaMangga'])
+print ("Harga Jeruk = ", dict['HargaJeruk'])
